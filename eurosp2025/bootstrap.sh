e95533a3951781d7e3ced81732a3c5b8031ff963
Jonas Vinck <jonas.vinck@kuleuven.be>
2024-10-30 17:35:19 +0100

    Updates for libc config and small config fix for allocator path.

--- eurosp2025/bootstrap.sh
+++ eurosp2025/bootstrap.sh
@@ -9,7 +9,7 @@ cd ../
 
 ./bootstrap.sh
 cd build/
-make enable-ipmon-pmvee && make benchmark && make -j 1>/dev/null
+make enable-ipmon-pmvee && ake block-shm && make benchmark && make -j 1>/dev/null
 cd ../
 
 cd eurosp2025/
@@ -23,7 +23,18 @@ then
     mkdir build-tree
     patch -d ./ -p 1 < ../patches/ReMon-glibc.patch
     cd build-tree
-    CFLAGS="-g -O2 -fno-builtin -Wno-alloca-larger-than -ggdb -fno-omit-frame-pointer" ../configure --enable-stackguard-randomization --enable-obsolete-rpc --enable-pt_chown --with-selinux --enable-lock-elision=no --enable-addons=nptl --prefix=/usr/ --sysconfdir=/etc/ --no-create --no-recursion
+    CFLAGS="-g -O2 -fno-builtin -Wno-alloca-larger-than -ggdb -fno-omit-frame-pointer" ../configure --enable-stackguard-randomization --enable-obsolete-rpc --enable-pt_chown --with-selinux --enable-lock-elision=no --enable-addons=nptl --prefix=/usr/ --sysconfdir=/etc/
+    make -j
+    # ln -fs $(readlink -f ./) ../../../patched_binaries/libc/amd64/
+    ln -fs $(readlink -f ./elf/ld-linux-x86-64.so.2) ../../../patched_binaries/libc/amd64/ld-linux.so # ld-2.31.so
+    ln -fs $(readlink -f ./libc.so) ../../../patched_binaries/libc/amd64/libc.so.6 # libc-2.31.so
+    ln -fs $(readlink -f ./dlfcn/libdl.so) ../../../patched_binaries/libc/amd64/libdl.so.2 # libdl-2.31.so
+    ln -fs $(readlink -f ./math/libm.so) ../../../patched_binaries/libc/amd64/libm.so.6 # libm-2.31.so
+    ln -fs $(readlink -f ./nptl/libpthread.so) ../../../patched_binaries/libc/amd64/libpthread.so.0 # libpthread-2.31.so
+    ln -fs $(readlink -f ./resolv/libresolv.so) ../../../patched_binaries/libc/amd64/libresolv.so.2 # libresolv-2.31.so
+    ln -fs $(readlink -f ./rt/librt.so) ../../../patched_binaries/libc/amd64/librt.so.1 # librt-2.31.so
+    ln -fs $(readlink -f ./login/libutil.so) ../../../patched_binaries/libc/amd64/libutil.so.1 # libutil-2.31.so
+
     cd ../../
 fi
 ## fortdivide changes to ReMon libc build and config
@@ -56,11 +67,12 @@ fi
 ## final benchmark setup
 sed -r -i "s.##benchmark_location##.$(readlink -f ./).g" ./pmvee_config/nginx-1.23.3/*
 sed -r -i "s.##benchmark_location##.$(readlink -f ./).g" ./pmvee_config/lighttpd-1.4.60/*
-sed -r -i "s.##benchmark_location##.$(readlink -f ./).g" ./pmvee_config/MVEE.ini.patch/*
+sed -r -i "s.##fortdivide_location##.$(readlink -f ../).g" ./pmvee_config/MVEE.ini.patch*
+
+ln -fs $(readlink -f ./pmvee_config/nginx-1.23.3/lib/nginx_l.so) ../patched_binaries/gnomelibs/amd64/
 
 cd ../
 patch -d ./ -p 1 < eurosp2025/pmvee_config/MVEE.ini.patch
-
 
 cd ./PMVEE/allocator/
 make
